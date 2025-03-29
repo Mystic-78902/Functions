1b605678eb7b2ccd271656a13f5d019b7770cd59
Mystic-78902 <charani.avula@gmail.com>
2025-03-29 17:04:15 +0530

    Python

---
+++ Cal peri.py
@@ -0,0 +1,5 @@
+def squareperi(squareside):
+    number = 4 * squareside
+    return number
+squareside = int(input("Enter the square side.."))
+print("The perimeter of a square is", squareperi(squareside))
---
+++ Well Wishes.py
@@ -0,0 +1,6 @@
+def well_wishes():
+    print("Hi")
+    print("I got a new QWO5 Keyboard.")
+
+
+well_wishes()
